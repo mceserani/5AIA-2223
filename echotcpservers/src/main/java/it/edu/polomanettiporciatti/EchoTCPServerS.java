33f8ee1fecb2164e445c85e1df25d0e405d88d1d
Matteo Ceserani <ceserani@gmail.com>
2022-09-23 23:31:02 +0200

    First commit

---
+++ echotcpservers/src/main/java/it/edu/polomanettiporciatti/EchoTCPServerS.java
@@ -0,0 +1,79 @@
+package it.edu.polomanettiporciatti;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoTCPServerS {
+	
+	private EchoTCPServerS(){
+	}
+
+	/**
+	 * 
+	 * @param args Command line arguments
+	 * @author Matteo Ceserani
+	 * @version 1.0
+	 */
+    public static void main(String[] args){
+       
+		ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(7);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 7.");
+            System.exit(1);
+        }
+
+        while(true){
+
+            Socket socket = null;
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                continue;
+            }
+
+            BufferedReader in = null;
+            try {
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            } catch (IOException e) {
+                System.err.println("Cannot create socket streams.");
+                continue;       
+            }
+
+            String inputLine = null;
+            try {
+                inputLine = in.readLine();
+            } catch (IOException e) {
+                System.err.println("Cannot read from socket.");
+                continue;
+            }
+
+            PrintWriter out = null;
+            try {
+                out = new PrintWriter(socket.getOutputStream(), true);
+            } catch (IOException e) {
+                System.err.println("Cannot create socket streams.");
+                continue;      
+            }
+
+            out.println(inputLine);
+
+            try {
+                socket.close();
+            } catch (IOException e) {
+                System.err.println("Cannot close socket.");
+                continue;
+            }
+        
+        } // end while
+
+
+    }
+
+}
---
+++ echotcpservers/src/test/java/it/edu/polomanettiporciatti/EchoTCPServerSTest.java
@@ -0,0 +1,20 @@
+package it.edu.polomanettiporciatti;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for EchoTCPServerS.
+ */
+class EchoTCPServerSTest {
+
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testEchoTCPServerS() {
+        assertEquals(1, 1);
+    }
+
+}
