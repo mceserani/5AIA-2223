de3393f472c1f7d521ee07302892fc32c9427f06
Matteo Ceserani <ceserani@gmail.com>
2022-09-23 23:34:45 +0200

    Other files

---
+++ echotcpclient/src/test/java/it/edu/polomanettiporciatti/EchoTCPClientTest.java
@@ -0,0 +1,20 @@
+package it.edu.polomanettiporciatti;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for EchoTCPClient.
+ */
+class EchoTCPClientTest {
+
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testEchoTCPClient() {
+        assertEquals(1, 1);
+    }
+
+}
