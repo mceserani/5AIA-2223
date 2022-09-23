e7cd5713d7e7ce7aa41cd2a54cf8e1ad6f04e9b6
Matteo Ceserani <ceserani@gmail.com>
2022-09-23 23:34:25 +0200

    First commit

---
+++ echotcpclient/src/main/java/it/edu/polomanettiporciatti/EchoTCPClient.java
@@ -0,0 +1,18 @@
+package it.edu.polomanettiporciatti;
+
+public class EchoTCPClient {
+	
+	private EchoTCPClient(){
+	}
+
+	/**
+	 * 
+	 * @param args Command line arguments
+	 * @author Matteo Ceserani
+	 * @version 1.0
+	 */
+    public static void main(String[] args){
+       
+    }
+
+}
